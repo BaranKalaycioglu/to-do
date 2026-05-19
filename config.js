e4223f3d9a0fddfabe86b15e24146520d40f2b88
BaranKalaycioglu <usbarank@gmail.com>
2026-05-19 11:48:44 -0400

    Change Supabase URL and anon key
    Updated Supabase project URL and anon key.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your Supabase project values here.
 // Supabase dashboard → Project Settings → API
-window.SUPABASE_URL = "https://zppmgsakejdiwxjhyila.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwcG1nc2FrZWpkaXd4amh5aWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NzAzNDcsImV4cCI6MjA5MzU0NjM0N30.qyXC6Bhw1JGcSb-jzsBTb3QfU7ZLhvFh8uJqA8O-b3E";
+window.SUPABASE_URL = "https://jhbbhtfawbsdicflahsl.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpoYmJodGZhd2JzZGljZmxhaHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxOTI5NzUsImV4cCI6MjA5NDc2ODk3NX0.FQhzaanC_Glc4dVzpog6eX5VaqA4lgc7k7OKdVAqdUA";
